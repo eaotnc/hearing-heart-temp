bffb76501a0a15d54a63a5387cbeb7214469febc
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-10-19 21:40:03 +0700

    feat: create nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,6 +10,33 @@
       </div>
       <div class="top-user-info">
         Thanatcha@gmail.com
+        <v-icon>fas fa-search</v-icon>
+      </div>
+    </div>
+    <div class="nav-section">
+      <nuxt-link to="/">
+        <img
+          class="main-logo"
+          src="../static/hearing-heart-logo.png"
+          alt="hearing-heart-logo"
+        />
+      </nuxt-link>
+      <div class="search-box">
+        <v-text-field
+          solo
+          label="ค้นหา..."
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </div>
+      <div class="nav-item-group">
+        <nuxt-link to="/" class="nav-item">
+          หน้าหนัก
+        </nuxt-link>
+        <nuxt-link to="/" class="nav-item">เวชพันธมิตร</nuxt-link>
+        <nuxt-link to="/" class="nav-item">วาระแห่งชาติ</nuxt-link>
+        <nuxt-link to="/" class="nav-item">สื่อ</nuxt-link>
+        <nuxt-link to="/" class="nav-item">เกี่ยวกับเรา</nuxt-link>
+        <nuxt-link to="/" class="nav-item">บทความ</nuxt-link>
       </div>
     </div>
     <v-main>
@@ -49,10 +76,10 @@ a {
   justify-content: space-between;
 }
 .top-left {
+  margin: 5px;
   display: flex;
   justify-content: space-between;
   font-size: 14px;
-  margin: 5px;
   color: rgb(63, 63, 63);
   cursor: pointer;
   text-decoration: underline;
@@ -67,5 +94,35 @@ a {
   height: 50px;
   background-color: #eca986;
   border-radius: 0 0 0 10px;
+  font-weight: 300;
+}
+.nav-section {
+  display: flex;
+  height: 90px;
+}
+.main-logo {
+  width: 150px;
+  margin: 0 20px;
+  margin-top: 15px;
+  height: 65px;
+}
+
+.search-box {
+  padding-top: 20px;
+  width: 200px;
+}
+.nav-item-group {
+  display: flex;
+  margin-left: auto;
+  padding: 20px;
+}
+.nav-item {
+  color: black;
+  font-size: 18px;
+  text-decoration: none;
+  margin: 0 20px;
+}
+.nav-item:hover {
+  color: burlywood;
 }
 </style>
